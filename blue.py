56ec42d7e136efe7d503c9f7937530d4cbc380a0
Shunpei Inoue <inoue.syunpei@akane.waseda.jp>
2018-01-07 00:39:39 +0900

    補正後画像取得

--- blue.py
+++ blue.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 #include "windows.h"
 import cv2
-import time
+import matplotlib.pyplot as plt
+import numpy as np
 
-img1 = cv2.imread("blue.png")
+def revision(pts1,pts2,img):
+    M = cv2.getPerspectiveTransform(pts1,pts2)
+    inv_M =np.linalg.inv(M)
+    dst = cv2.warpPerspective(img,inv_M,(400,220))
+    return dst
 
+img1 = cv2.imread("blue2.png",1)
+#img1 = cv2.resize(img,(400,200))
 #フルスクリーン表示
 cv2.namedWindow("img", cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty("img", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
+#cv2.setWindowProperty("img", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
+pts1=np.float32([[76,61],[282,61],[119,178],[240,178]])
+pts2=np.float32([[119,58],[240,58],[119,178],[240,178]])
+dst = revision(pts1,pts2,img1)
 
-cv2.imshow("img",img1)
-cv2.waitKey(500)
+plt.subplot(121),plt.imshow(img1),plt.title('Input')
+plt.subplot(122),plt.imshow(dst),plt.title('Output1')
+plt.show()
+#cv2.imshow("img",img1)
+#cv2.waitKey(-1)
--- daikei2.py
+++ daikei2.py
@@ -1,17 +1,33 @@
 #coding: UTF-8
 import cv2
 import numpy as np
+import time
 
-cap = cv2.VideoCapture(0)
+#オリジナル画像に対応した台形座標取得
+def getPts(areas):
+    pts1 = np.float32([[120,36],[280,36],[120,190],[280,190]])
+    #スケール変換変換
+    k = (pts1[3][0]-pts1[2][0])/(areas[0][3][0][0]-areas[0][2][0][0])
+    h1 = (areas[0][1][0][1]-areas[0][2][0][1])*k
+    h2 = (areas[0][0][0][1]-areas[0][3][0][1])*k
+    delta1 = (areas[0][2][0][0]-areas[0][1][0][0])*k
+    delta2 = (areas[0][0][0][0]-areas[0][3][0][0])*k
+    x1 = pts1[2]+[-delta1,-h1]
+    x2 = pts1[3]+[delta2,-h2]
+    pts2 = np.float32([x1,x2,pts1[2],pts1[3]])
+    return pts1,pts2
 
-count = 0
-while count<100:
+#台形補正画像を生成する関数
+def revision(pts1,pts2,img):
+    M = cv2.getPerspectiveTransform(pts1,pts2)
+    inv_M =np.linalg.inv(M)
+    dst = cv2.warpPerspective(img,inv_M,(600,400))
+    return dst
 
-    # Take each frame
-    _, frame = cap.read()
-
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+#青の輪郭を取ってくる関数
+def getBlue(image):
+    # Convert BGR to HSV and smooth
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     smooth=cv2.GaussianBlur(hsv,(15,15),0)
 
     # define range of blue color in HSV
@@ -29,19 +45,40 @@ while count<100:
     cnt = contours[0]
     epsilon = 0.08*cv2.arcLength(cnt,True)
     approx = cv2.approxPolyDP(cnt,epsilon,True)
-    areas.append(np.array(approx))
-    #for contour in contours:
-    #    epsilon = 0.08*cv2.arcLength(contour,True)
-    #    approx = cv2.approxPolyDP(contour,epsilon,True)
-    #    areas.append(np.array(approx))
+    # areas.append(np.array(approx))
+    areas.append(approx)
+    return (areas,res)
 
-    cv2.drawContours(res, areas, -1, (0,0,255), 3)
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    count +=1
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
+img = cv2.imread("blue.png",1)
+cv2.namedWindow("img", cv2.WND_PROP_FULLSCREEN)
+
+
+capture = cv2.VideoCapture(0)
+
+count = 0
+# isOpenedの代わりにTrueを使うと，frameがemptyのときエラーを吐く
+while capture.isOpened():
+    ret, frame = capture.read()
+
+    if ret :
+        areas,res = getBlue(frame)
+        cv2.drawContours(res, areas, -1, (0,0,255), 3)
+        cv2.imshow('frame',frame)
+        cv2.imshow('res',res)
+        time.sleep(2)
+        if len(areas[0])==4 :
+            pts1,pts2 = getPts(areas)
+            dst = revision(pts1,pts2,img)
+            break
+
+    if cv2.waitKey(1) &  0xFF == ord('q'):
         break
 
+capture.release()
+cv2.imshow('img',dst)
+cv2.waitKey(-1)
+
 cv2.destroyAllWindows()
+
+# cv2.imshow('img',img)
+# cv2.waitKey(-1)
