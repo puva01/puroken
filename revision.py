17f7c9fd67322ba3371570e0177bb53ac71dae68
Shunpei Inoue <inoue.syunpei@akane.waseda.jp>
2018-01-12 04:35:19 +0900

    台形補正画像

--- revision.py
+++ revision.py
@@ -20,22 +20,30 @@ pts3 = np.float32([[125-5,75-5],[160+5,75-5],[125-5,114+5],[160+5,114+5]])
 #台形
 pts4 = np.float32([[120,60],[165,60],[125,114],[160,114]])
 
+#四角形のサイズを小さくし，移動させてみる
+pts1 = np.float32([[50,50],[70,50],[50,70],[70,70]])
+pts2 = np.float32([[45,45],[75,45],[50,70],[70,70]])
+
+dst = revision(pts1,pts2,img)
+
 dst0 = revision(pts0,pts1,img)
 dst1 = revision(pts0,pts2,img)
 dst2 = revision(pts0,pts3,img)
 dst3 = revision(pts0,pts4,img)
-plt.subplot(231),plt.imshow(img),plt.title('Input')
-plt.subplot(232),plt.imshow(dst0),plt.title('Output0')
-plt.subplot(233),plt.imshow(dst1),plt.title('Output1')
-plt.subplot(234),plt.imshow(dst2),plt.title('Output2')
-plt.subplot(235),plt.imshow(dst3),plt.title('Output3')
-plt.show()
-
+# plt.subplot(231),plt.imshow(img),plt.title('Input')
+# plt.subplot(232),plt.imshow(dst0),plt.title('Output0')
+# plt.subplot(233),plt.imshow(dst1),plt.title('Output1')
+# plt.subplot(234),plt.imshow(dst2),plt.title('Output2')
+# plt.subplot(235),plt.imshow(dst3),plt.title('Output3')
+# plt.subplot(236),plt.imshow(dst),plt.title('Output4')
+# plt.show()
+cv2.imshow("img",dst3)
+cv2.waitKey(-1)
 '''
 結果
 ･射影変換は，移動先(pts2)に向かって平行移動する．したがって，射影行列の逆行列を使うと，pts2の平行移動と逆方向に平行移動する
 ･拡大に対しては拡大(逆行列では縮小)．
 ･画像中の部分的な4点の射影変換に対し，画像全体が歪み修正される．
 結論
-.pts1,pts2の面積および位置関係は大体同様である必要がある．
+.pts1,pts2の面積および位置関係は大体同様にする必要がある．
 '''
