941c8f16d60e092b00903994c05211305ca857b9
Shunpei Inoue <inoue.syunpei@akane.waseda.jp>
2018-01-13 19:22:07 +0900

    daikei2:パワポ用，daikei3:実装用

--- daikei2.py
+++ daikei2.py
@@ -6,22 +6,52 @@ import time
 from operator import itemgetter
 
 #オリジナル画像に対応した台形座標取得
-def getPts(areas):
-    #ブルーの元画像の頂点．findContoursで見つけてくる．
-    img_area,_,_ = getTarget(img)
-    x00=img_area[0][0][0][0]
-    y00=img_area[0][0][0][1]
-    x11=img_area[0][1][0][0]
-    y11=img_area[0][1][0][1]
-    x22=img_area[0][2][0][0]
-    y22=img_area[0][2][0][1]
-    x33=img_area[0][3][0][0]
-    y33=img_area[0][3][0][1]
-    p00 = [x00,y00]#左上
-    p11 = [x11,y11]#左下
-    p22 = [x22,y22]#右下
-    p33 = [x33,y33]#右上
-    pts1 = np.float32([p00,p33,p11,p22]) #areasと同じ順　(左上，右上，左下，右下)
+# def getPts(areas):
+#     #ブルーの元画像の頂点．findContoursで見つけてくる．
+#     img_area,_,_ = getTarget(img)
+#     x00=img_area[0][0][0][0]
+#     y00=img_area[0][0][0][1]
+#     x11=img_area[0][1][0][0]
+#     y11=img_area[0][1][0][1]
+#     x22=img_area[0][2][0][0]
+#     y22=img_area[0][2][0][1]
+#     x33=img_area[0][3][0][0]
+#     y33=img_area[0][3][0][1]
+#     p00 = [x00,y00]#左上
+#     p11 = [x11,y11]#左下
+#     p22 = [x22,y22]#右下
+#     p33 = [x33,y33]#右上
+#     pts1 = np.float32([p00,p33,p11,p22]) #areasと同じ順　(左上，右上，左下，右下)
+#     #areasの格納が普通の配列じゃないので，配列に書き直す．
+#     x0=areas[0][0][0][0]
+#     y0=areas[0][0][0][1]
+#     x1=areas[0][1][0][0]
+#     y1=areas[0][1][0][1]
+#     x2=areas[0][2][0][0]
+#     y2=areas[0][2][0][1]
+#     x3=areas[0][3][0][0]
+#     y3=areas[0][3][0][1]
+#     p0 = [x0,y0]
+#     p1 = [x1,y1]
+#     p2 = [x2,y2]
+#     p3 = [x3,y3]
+#     area = [p0,p1,p2,p3]
+#     #areasの4つの座標を左上，右上，左下，右下の順で並び替える．台形の場合x座標の順で一意に決定
+#     #np.float32リストに対してitemgetterは使えないらしい
+#     area.sort(key=itemgetter(0))
+#     #スケール変換変換
+#     k = (pts1[3][0]-pts1[2][0])/(area[3][0]-area[1][0])
+#     h1 = (area[0][1]-area[1][1])*k
+#     h2 = (area[3][1]-area[2][1])*k
+#     delta1 = (area[0][0]-area[1][0])*k
+#     delta2 = (area[2][0]-area[3][0])*k
+#     x1 = pts1[2]+[-delta1,-h1]
+#     x2 = pts1[3]+[delta2,-h2]
+#     pts2 = np.float32([x1,x2,pts1[2],pts1[3]])
+#     return pts1,pts2,x1,x2,k,delta1,h1,h2,area
+
+def calibration(M,areas):
+    pts1 = np.float32([[446,171],[790,173],[446,514],[788,514]]) #areasと同じ順　(左上，右上，左下，右下)
     #areasの格納が普通の配列じゃないので，配列に書き直す．
     x0=areas[0][0][0][0]
     y0=areas[0][0][0][1]
@@ -40,21 +70,19 @@ def getPts(areas):
     #np.float32リストに対してitemgetterは使えないらしい
     area.sort(key=itemgetter(0))
     #スケール変換変換
-    k = (pts1[3][0]-pts1[2][0])/(area[3][0]-area[1][0])
-    h1 = (area[0][1]-area[1][1])*k
-    h2 = (area[3][1]-area[2][1])*k
-    delta1 = (area[0][0]-area[1][0])*k
-    delta2 = (area[2][0]-area[3][0])*k
-    x1 = pts1[2]+[-delta1,-h1]
-    x2 = pts1[3]+[delta2,-h2]
+    k = (pts1[3][0]-pts1[2][0])/(area[2][0]-area[1][0])
+    h = (area[1][1]-area[0][1])*k
+    delta = (area[1][0]-area[0][0])*k
+    x1 = pts1[2]+[-delta,-h1]
+    x2 = pts1[3]+[delta,-h2]
     pts2 = np.float32([x1,x2,pts1[2],pts1[3]])
-    return pts1,pts2,x1,x2,k,delta1,h1,h2,area
+    M = cv2.getPerspectiveTransform(pts1,pts2)
+    return M
 
 #台形補正画像を生成する関数
-def revision(pts1,pts2,img):
-    M = cv2.getPerspectiveTransform(pts1,pts2)
+def revision(M,img):
     inv_M =np.linalg.inv(M)
-    dst = cv2.warpPerspective(img,inv_M,(1000,800))
+    dst = cv2.warpPerspective(img,inv_M,(0,800))
     return dst
 
 #ターゲット(黄色)の輪郭を取ってくる関数
@@ -62,21 +90,36 @@ def getTarget(image):
     # Convert BGR to HSV and smooth
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     smooth=cv2.GaussianBlur(hsv,(15,15),0)
-
-    # define range of blue color in HSV　(第1引数を110〜130→90〜140に変更)
-
-    #lower_blue = np.array([90,50,50])
-    #upper_blue = np.array([140,255,255])
-
     #黄色
     lower_blue = np.array([25,100,85])
     upper_blue = np.array([100,255,255])
-    #赤
-    # h = hsv[:, :, 0]
-    # s = hsv[:, :, 1]
-    # mask = np.zeros(h.shape, dtype=np.uint8)
-    # mask[((h < 30) | (h > 180)) & (s > 100)] = 255
 
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(smooth, lower_blue, upper_blue)
+
+    # Bitwise-AND mask and original image(白黒画像の中で，白の部分だけ筒抜けになって映る)
+    res = cv2.bitwise_and(image,image, mask= mask)
+    image,contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    areas = []
+    contours.sort(key=cv2.contourArea,reverse=True)
+    if  contours:
+        cnt = contours[0]
+        epsilon = 0.08*cv2.arcLength(cnt,True)
+        approx = cv2.approxPolyDP(cnt,epsilon,True)
+        areas.append(approx)
+    else:
+        cnt = []
+    return areas,res,cnt,
+
+#
+def getBlue(frame):
+    # Convert BGR to HSV and smooth
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    smooth=cv2.GaussianBlur(hsv,(15,15),0)
+
+    # define range of blue color in HSV　(第1引数を110〜130→90〜140に変更)
+    lower_blue = np.array([90,50,50])
+    upper_blue = np.array([140,255,255])
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(smooth, lower_blue, upper_blue)
